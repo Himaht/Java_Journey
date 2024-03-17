7d855fa33e9b570459b110730d0c526e51748a95
Rahimatu Yushawu <76118023+Himaht@users.noreply.github.com>
2024-03-17 18:11:31 -0500

    commit

--- MovieRental/MovieRentalSystem.java
+++ MovieRental/MovieRentalSystem.java
@@ -5,28 +5,40 @@ public class MovieRentalSystem {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		
-		Movie movies = new Movie();
-		Customer customers = new Customer();
+		//Movie movies = new Movie();
+		//Customer customers = new Customer();
 		
 		String movieTitle, movieGenre, movieMirector, contactInfo, MovieName;
 		
 		int movieRealeaseyear;
 		
+		System.out.println("Enter Movie Title");
 		movieTitle = scnr.nextLine();
+		System.out.println("Enter Movie Genre");
 		movieGenre = scnr.nextLine();
+		System.out.println("Enter Movie Director");
 		movieMirector = scnr.nextLine();
+		System.out.println("Enter Customer's Contact Information");
 		contactInfo = scnr.nextLine();
+		System.out.println("Enter customer's name");
 		MovieName = scnr.nextLine();
 		
+		System.out.println("Enter realease year");
 		movieRealeaseyear = scnr.nextInt();
 		
 		Movie movies = new Movie(movieTitle, movieMirector, movieGenre, movieRealeaseyear);
 		
+		Customer customers = new Customer(MovieName, contactInfo);
+		
+		customers.rentedMovielist(movies);
+		
+		customers.getrentedMovie();
+		
 		System.out.println(movies);
 
-		movie.availability();
+		movies.availability();
 
-        	movie.returnedMovie();
+        	movies.returnedMovie();
 	}
 
 }
